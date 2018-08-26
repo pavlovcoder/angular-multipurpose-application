b4cee125dab22b128f9d0b4583ac7f9d896fb208
Vladimir <pavlovcoder@gmail.com>
2018-08-26 18:01:24 +0200

    Commit #8

--- src/app/form.model.ts
+++ src/app/form.model.ts
@@ -1,4 +1,5 @@
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LimitValidator } from './limit.formvalidator';
 
 export class ProductFormControl extends FormControl {
   label: string;
@@ -24,6 +25,8 @@ export class ProductFormControl extends FormControl {
           case 'maxLength':
             messages.push(`A ${this.label} must be no more than ${this.errors['minLength'].requiredLength} characters.`);
             break;
+          case 'limit':
+            messages.push(`A ${this.label} cannot be more than ${this.errors['limit'].limit}`);
           case 'pattern':
             messages.push(`The ${this.label} contains illegal characters.`);
         }
@@ -48,6 +51,7 @@ export class ProductFormGroup extends FormGroup {
       ])),
       price: new ProductFormControl("Price", "price", "", Validators.compose([
         Validators.required,
+        LimitValidator.Limit(100),
         Validators.pattern("^[0-9\.]+$")
       ]))
     });
