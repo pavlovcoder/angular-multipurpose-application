ad46ab24541aff9bdd4c8bdf28c0e9839b57001c
Vladimir Pavlov <pavlovcoder@gmail.com>
2018-09-22 16:42:47 +0200

    Commit #12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProductComponent } from './component';
 import { HighlightDirective } from './highlight.directive';
 import { ExponentialStrengthPipe } from './exponential-strength.pipe';
 import { FlyingHeroesPipe } from './flying-heroes.pipe';
+import { OpenCloseAnimationComponent } from './open-close-animation/open-close-animation.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
-  declarations: [ProductComponent, HighlightDirective, ExponentialStrengthPipe, FlyingHeroesPipe],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, BrowserAnimationsModule],
+  declarations: [ProductComponent, HighlightDirective, ExponentialStrengthPipe, FlyingHeroesPipe, OpenCloseAnimationComponent],
   bootstrap: [ProductComponent]
 })
 
---
+++ src/app/open-close-animation/open-close-animation.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+@Component({
+  selector: 'app-open-close-animation',
+  templateUrl: './open-close-animation.component.html',
+  styleUrls: ['./open-close-animation.component.css'],
+  animations: [
+    
+  ]
+})
+export class OpenCloseAnimationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
