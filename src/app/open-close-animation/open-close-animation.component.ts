8e4d6b4be03d926733f10f056bb12c2034f95d68
Vladimir Pavlov <pavlovcoder@gmail.com>
2018-10-02 19:41:13 +0200

    Commit #13

--- src/app/open-close-animation/open-close-animation.component.ts
+++ src/app/open-close-animation/open-close-animation.component.ts
@@ -6,14 +6,40 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   templateUrl: './open-close-animation.component.html',
   styleUrls: ['./open-close-animation.component.css'],
   animations: [
-    
-  ]
+    trigger('openClose', [
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.5,
+        backgroundColor: 'green'
+      })),
+      transition('* => closed', [
+        animate('1s')
+      ]),
+      transition('* => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
 })
 export class OpenCloseAnimationComponent implements OnInit {
+  public isOpen = true;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public toggleStatus(): void {
+    this.isOpen = !this.isOpen;
+    console.log(this.isOpen);
+  }
+
 }
+
+// If the Angular Live Server isn't working, please using the command below for running on the watching mode:
+// ng serve --poll=1000 --port 3000 --open
