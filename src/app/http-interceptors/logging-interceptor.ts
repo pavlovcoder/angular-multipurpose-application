629288f9f62951ec61d531f557f5702ce66e88a3
Vladimir Pavlov <pavlovcoder@gmail.com>
2018-10-14 21:44:58 +0200

    Commit #17

---
+++ src/app/http-interceptors/logging-interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse } from '@angular/common/http';
+import { finalize, tap } from 'rxjs/operators';
+import { MessageService } from '../message.service';
+
+@Injectable()
+export class LoggingInterceptor implements HttpInterceptor {
+    constructor(private messenger: MessageService) {}
+
+    public intercept(req: HttpRequest<any>, next: HttpHandler) {
+        const started = Date.now();
+        let ok: string;
+
+        return next.handle(req)
+            .pipe(
+                tap(
+                    event => ok = event instanceof HttpResponse ? 'succeeded' : '',
+                    error => ok = 'failed'
+                ),
+                finalize(() => {
+                    const elapsed = Date.now() - started;
+                    const msg = `${req.method} "${req.urlWithParams}" ${ok} in ${elapsed} ms.`;
+                    this.messenger.add(msg);
+                })
+            );
+    }
+}
